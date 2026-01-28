5f7dec8ec8835c8ec191a713fb26846c6a59121c
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-28 20:40:37 +0530

    actions

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -6,7 +6,9 @@ if [ $NUMBER -gt 20 ]; then
     echo "Given Number: $NUMBER is greater than 20"
  elif [ $NUMBER -eq 20 ]; then
    echo "Given number :$NUMBER is equal to 20"
+ elif [ $NUMBER -ne 20 ]; then
+    echo "Given number :$Number not equal to 30"
  else
-    echo "Given Number: $NUMBER is less  than 20"
+    echo "Given Number: $NUMBER is less than 20"
 
 fi
