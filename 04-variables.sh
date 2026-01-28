196e79960aa53b4bba4c7e50fc6a229478bec8cc
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-28 12:57:34 +0530

    actions

---
+++ 04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$1:Hi $2,How are you"
+echo "$2:Hello $1,I am fine,what about you?"
+echo "$1:I am dooing good,what are you doing?"
+echo "$2:I am  learning Devsecops with aws course"
