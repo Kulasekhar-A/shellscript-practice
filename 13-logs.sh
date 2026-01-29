568586a70549ed9e3c5959b562634c38ba95c09b
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-29 16:38:22 +0530

    actions

--- 13-logs.sh
+++ 13-logs.sh
@@ -14,10 +14,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then
-       echo "$2...  FAILURE"
+       echo "$2..  FAILURE"
        exit 1
    else
-       echo "$2...  SUCCESS"
+       echo "$2..  SUCCESS"
    fi
 }
 
