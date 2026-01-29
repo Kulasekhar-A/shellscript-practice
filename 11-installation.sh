06dcb83f4ab1a84116cbd14a46af0a9e54eaa67b
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-29 17:04:43 +0530

    actions

--- 11-installation.sh
+++ 11-installation.sh
@@ -11,25 +11,25 @@ echo "Installing NodeJS"
 dnf install nodejs -y
 
 if [ $? -ne 0 ]; then
-  echo " Installing NodeJS..FAILURE"
+  echo " Installing NodeJS.. FAILURE"
   exit 1
 else
-  echo " Installing NodeJS..SUCCESS"
+  echo " Installing NodeJS.. SUCCESS"
 fi
 
-dnf install mysql-server  -y
+dnf install mysql-server -y
 
 if [ $? -ne 0 ]; then
-  echo " Installing MySQL..FAILURE"
+  echo " Installing MySQL.. FAILURE"
   exit 1
 else
-  echo " Installing MySQL..SUCCESS"
+  echo " Installing MySQL.. SUCCESS"
 fi
 
 dnf install nginx -y
 if [ $? -ne 0 ]; then
-  echo " Installing nginx..FAILURE"
+  echo " Installing nginx.. FAILURE"
   exit1
 else
-  echo " Installing Nginx..SUCCESS"
+  echo " Installing Nginx.. SUCCESS"
 fi
