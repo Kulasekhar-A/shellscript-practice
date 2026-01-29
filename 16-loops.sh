afe33f199104252ccd35fdc349a3b50c183cc1dc
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-29 19:44:35 +0530

    actions

---
+++ 16-loops.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo "Please run this script with root user access" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p  $LOGS_FOLDER
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+       echo " $2 ... FAILURE" | tee -a $LOGS_FILE
+       exit 1
+   else
+       echo " $2 ... SUCCESS" | tee -a $LOGS_FILE
+   fi
+}
+
+for package in $@
+do
+  dnf list installed $package &>> $LOGS_FILE
+  if [ $? -ne o ]; then
+     echo "$package not installed,installing now"
+     dnf install $package -y &>> $LOGS_FILE
+     VALIDATE $? "package installation"
+  else
+     echo "Already installed,skipping now"
+  fi
+      
+
+done
