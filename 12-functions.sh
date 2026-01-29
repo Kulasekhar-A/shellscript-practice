b8759a7ca8ef061c91a8ed86a18c42910caba417
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-29 12:31:29 +0530

    actions

---
+++ 12-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo "Please run this script with root user access"
+  exit 1
+fi
+
+VALIDATE(){
+if [ $1 -ne o ]; then
+  echo " Installing $2..FAILURE"
+  exit1
+else
+  echo " Installing $2..SUCCESS"
+fi
+}
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs"
+
+dnf install mysql -y
+VALIDATE $? "Installing MySQL"
+
+dnf insstall nginx -y
+VALIDATE $? "Installing nginx"
