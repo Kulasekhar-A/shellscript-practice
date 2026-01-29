4631a3fe210deacae1bd423f2114239b8faec488
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-29 21:03:38 +0530

    actions

---
+++ 20-trap.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e
+trap 'echo"There is an error in $LINENO, Command : $BASH_COMMAND"' ERR
+
+LOGS_FOLDER="/var/log/shell-script"
+LOGS_FILE="/var/log/shell-script/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo -e "$R Please run this script with root user access $N" | tee -a $LOGS_FILE
+  exit 1
+fi
+
+mkdir -p  $LOGS_FOLDER
+
+
+
+for package in $@
+do
+  dnf list installed $package &>> $LOGS_FILE
+  if [ $? -ne 0 ]; then
+     echo "$package not installed,installing now"
+     dnf install $package -y &>> $LOGS_FILE
+    
+  else
+     echo -e "$package Already installe ... $Y skipping now $N"
+  fi
+done
