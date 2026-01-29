a1d45cb757183266b73fcea36d779775eda898da
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-29 16:28:34 +0530

    actions

--- 13-logs.sh
+++ 13-logs.sh
@@ -24,7 +24,7 @@ VALIDATE(){
 dnf install nginx -y &>> $LOGS_FILE
 VALIDATE $? "Installing Nginx"
 
-dnf install mysql -y &>> $LOGS_FILE
+dnf install mysql-server -y &>> $LOGS_FILE
 VALIDATE $? "Installing Mysql"
 
 dnf install nodejs -y &>> $LOGS_FILE
