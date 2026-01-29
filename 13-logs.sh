32df070bb829f69c52b1b7d530d4fd66125f5c00
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-29 17:33:38 +0530

    actions

--- 13-logs.sh
+++ 13-logs.sh
@@ -6,7 +6,7 @@ LOGS_FILE="/var/log/shell-script/$0.log"
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-  echo "Please run this script with root user access" | tee -a $LOGS_FIILE
+  echo "Please run this script with root user access" | tee -a $LOGS_FILE
   exit 1
 fi
 
@@ -14,7 +14,7 @@ mkdir -p  $LOGS_FOLDER
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then
-       echo "$2 ... FAILURE" | tee -a $LOGS_FIILE
+       echo "$2 ... FAILURE" | tee -a $LOGS_FILE
        exit 1
    else
        echo "$2 ... SUCCESS" | tee -a $LOGS_FILE
