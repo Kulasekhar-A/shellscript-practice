579e3035228d98c89428a5b7368851579664bec7
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-01-31 16:30:12 +0530

    actions

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "Kulasekhar:Hi Sivaji,How are you"
+echo "Kulasekhar:Hi Sivaji , How are you"
 echo "Sivaji:Hello Kulasekhar,I am fine,what about you?"
 echo "Kulasekhar:I am dooing good,what are you doing?"
 echo "Sivaji:I am  learning Devsecops with aws course"
+echo "Kulasekhar:I am learning aws"
