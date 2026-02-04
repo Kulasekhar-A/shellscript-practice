fa72a6ee803d4ccd8d7f215e7a2c782c95f1cf25
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-04 19:41:41 +0530

    actions

---
+++ 21-delete-old-logs.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LOGS_DIR=/home/ec2-user/app-logs
+LOGS_FILE="/$LOGS_DIR/$0.log"
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+#check the directory it is present or not
+if [ ! -d $LOGS_DIR ]; then
+   echo -e "$LOGS_DIR does not exist"
+   exit 1
+fi
+
+#find the files in particular directory
+FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
+echo "$FILES_TO_DELETE"
