ac3b05b06ff25c6a2f804c48cc68fafc1f1a0e17
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-04 19:54:08 +0530

    actions

--- 21-delete-old-logs.sh
+++ 21-delete-old-logs.sh
@@ -17,3 +17,9 @@ fi
 #find the files in particular directory
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
 echo "$FILES_TO_DELETE"
+
+while IFS= read -r FILE_PATH
+do
+  # Process the line here
+  echo "$FILE_PATH"
+done <<< $FILES_TO_DELETE
