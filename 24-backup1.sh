3ff020918f42847a7039fd6a1ab2da5270eece88
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-05 18:07:28 +0530

    actions

--- 24-backup1.sh
+++ 24-backup1.sh
@@ -10,7 +10,7 @@ N="\e[0m"
 
 SOURCE_DIR=$1
 DEST_DIR=$2
-DAYS=${3:-14}
+DAYS=${3:-14} #14 days is the default value , if the user not supplied
 
 USERID=$(id -u)
 
@@ -31,25 +31,31 @@ log(){
 USAGE(){
 
     log "$R USAGE : sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS> [ default 14 days ]$N"
+    exit 1
 }
 
 if [ $# -lt 2 ]; then
   USAGE
 fi
 
+#to check source_dir is present or not
 if [ ! -d $SOURCE_DIR ]; then
    log "$R source directory : $SOURCE_DIR does not exist$N"
    exit 1
 fi 
 
+#to check destination_dir is present or not
 if [ ! -d $DEST_DIR ]; then
    log "$R destination directory : $DEST_DIR does not exist$N"
    exit 1
 fi 
 
+#find the files
 FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
 
 log "backup started"
 log "source directory : $SOURCE_DIR"
 log "destination directory :$DEST_DIR"
 log "days : $DAYS"
+
+if [ -z ]
