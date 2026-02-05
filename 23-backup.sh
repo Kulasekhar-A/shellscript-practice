98406a5fd7a5ca89486a64756221db0b438088e8
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-05 12:26:38 +0530

    actions

--- 23-backup.sh
+++ 23-backup.sh
@@ -20,6 +20,11 @@ fi
 
 mkdir -p  $LOGS_FOLDER
 
+log(){
+
+    echo -e "$(date "+%y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
 USAGE(){
 
     echo -e " $R USAGE :: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS> [ default 14 days ]$N"
@@ -30,11 +35,22 @@ if [ $# -lt 2 ]; then
 fi
 
 if [ ! -d $SOURCE_DIR ]; then
- echo -e "$R source directory :: $SOURCE_DIR does not exist $N"
+ echo -e "$R source directory : $SOURCE_DIR does not exist $N"
  exit 1
 fi
 
 if [ ! -d $DEST_DIR ]; then
- echo -e "$R destination drectory :: $DEST_DIR does not exist $N"
+ echo -e "$R destination drectory : $DEST_DIR does not exist $N"
  exit 1
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+
+log "backup started"
+log "source directory : $SOURCE_DIR"
+log "destination directory : $DEST_DIR"
+log "Days : $DAYS"
+
+if [ -z $FILES ]; then
+  log "no files to archieve : $Y skipping ... $N"
 fi
