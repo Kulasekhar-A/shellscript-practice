c44d39d962e9b43433c30f0fae500610451bfc68
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-06 15:48:47 +0530

    actions

---
+++ 24-disk-usage.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MESSAGE=""
+
+log(){
+
+    echo -e "$(date "+%y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
+}
+
+DISK_USAGE=$(df -hT | grep -v Filesystem)
+USAGE_THRESHOLD=3
+
+while IFS = read -r line
+do
+  USAGE=$(df -hT | grep -v Filesystem | awk '{print $6}' | cut -d "%" -f1)
+  PARTITION=$(df -hT | grep -v Filesystem | awk '{print $7}')
+
+  if [ $USAGE -gt $USAGE_THRESHOLD ]; then
+     MESSAGE="High disk usage on $PARTITION : $USAGE"
+fi
+done <<< $DISK_USAGE
+
+log "$MESSAGE"
