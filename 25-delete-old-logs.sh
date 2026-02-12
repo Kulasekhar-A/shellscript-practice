7374f8e3571675f4070b39bd7f79ef4c1b934bc5
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-12 14:46:25 +0530

    action

---
+++ 25-delete-old-logs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+LOGS_DIR=var/log/myapp
+LOGS_FILE="/$LOGS_DIR/$0.log"
+
+if [ ! -d $LOGS_DIR ]; then
+    echo "$LOGS_DIR"
+fi
+
+FILES_TO_DELETE=$(find $LOGS_DIR -name type f -mtime +14)
+echo "$FILES_TO_DELETE"
+
+while IFS=read -r FILE_PATH
+do
+ echo "delete file: $FILE_PATH"
+ rm -r $FILE_PATH
+ echo "delete file: "$FILE_PATH
+done <<< $FILES_TO_DELETE
