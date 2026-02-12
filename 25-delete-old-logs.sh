e8d3790e0698689f566c046ca8ce3cee8955ce18
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-12 18:17:01 +0530

    action

--- 25-delete-old-logs.sh
+++ 25-delete-old-logs.sh
@@ -4,7 +4,8 @@ LOGS_DIR=var/log/myapp
 LOGS_FILE="/$LOGS_DIR/$0.log"
 
 if [ ! -d $LOGS_DIR ]; then
-    echo "$LOGS_DIR"
+    echo "$LOGS_DIR does not exist"
+    exit 1
 fi
 
 FILES_TO_DELETE=$(find $LOGS_DIR -name "*.log" -mtime +14)
@@ -13,6 +14,6 @@ echo "$FILES_TO_DELETE"
 while IFS=read -r FILE_PATH
 do
  echo "delete file: $FILE_PATH"
- rm -r $FILE_PATH
- echo "delete file: "$FILE_PATH
+ rm -f $FILE_PATH
+ echo "delete file: $FILE_PATH"
 done <<< $FILES_TO_DELETE
