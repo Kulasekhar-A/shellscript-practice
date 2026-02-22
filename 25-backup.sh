7e8f63f4d48d505a1f9a7bddc9750546aa0d767e
Kulasekhar Annuru <annurukulasekhar060@gmail.com>
2026-02-22 20:36:12 +0530

    actions

--- 25-backup.sh
+++ 25-backup.sh
@@ -10,37 +10,35 @@ DAYS=${3:-14}
 USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
-echo "please run this script with root user access"
+echo "please run the script with root user access"
 fi
 
 mkdir -p $LOGS_FOLDER
 
-log(){
-
-    echo "$(date "+%y-%m_%d %H-%M-%S") | $1" | tee -a $LOGS_FILE
+log() {
+    log "$(date "+%y-%m-%d %H:%M:%S") | $1" | tee -a $LOGS_FILE
 }
 
-USAGE () {
-
-    log "USAGE :: sudo backup <SOURCE_DIR> <DEST_DIR> <DAYS> [ default 14 days ]"
+USAGE(){
+    log "USAGE :: sudo backu <SOURCE_DIR> <DEST_DIR> <DAYS> [ default 14 days ]"
     exit 1
 }
 
-if [ $#  -lt 2 ]; then
+if [ $# -lt 2 ]; then
 USAGE
 fi
 
 if [ ! -d $SOURCE_DIR ]; then
-log "source directory : $SOURCE_DIR does not exist"
-exit 1
+ log "source directory : $SOURCE_DIR does not exist"
+ exit 1
 fi
 
 if [ ! -d $DEST_DIR ]; then
-log " destination directory : $DEST_DIR does not exist"
-exit 1
+ log "destination directory : $DEST_DIR does not exist"
+ exit 1
 fi
 
-FILES=$(find "$SOURCE_DIR" -name "*.log" -type f -mtime +"$DAYS")
+FILES=$(find $SOURCE_DIR -name "*.log" -type f -mtim e +$DAYS)
 
 log "backup started"
 log "source directory : $SOURCE_DIR"
@@ -48,23 +46,20 @@ log "destination directory : $DEST_DIR"
 log "Days : $DAYS"
 
 if [ -z "${FILES}" ]; then
-log "no files to archive : skipping"
+ log "no files to archive : skipping"
 else
-log "files found to archive : $FILES"
-TIMESTAMP=$(date +%F-%H-%M-%S)
-ZIP_FILE_NAME="$DEST_DIR/myapp-$TIMESTAMP.tar.gz"
-tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +"$DAYS")
-
-if [ -f $ZIP_FILE_NAME ]; then
-log "archiving is : success"
-while IFS= read -r file
-do
-log "delete file : $file"
-rm -f $file
-log "delete file : $file"
-done <<< $FILES
-else
-    log "Already archieving is ... $F failure $N"
-    exit 1
-fi
+ log "files found to archiive : $FILES"
+ TIMESTAMP=$(date +%F-%H-%M-%S)
+ ZIP_FILE_NAME="$DEST_DIR/myapp-$TIMESTAMP.tar.gz"
+ tar -zcvf $ZIP_FILE_NAME $(find $SOURCE_DIR -name "*.log" -type f -mtime +$DAYS)
+ if [ -f $ZIP_FILE_NAME ]; then
+  log "archiving is : success"
+  while IFS= read -r filepath
+  do
+  log "deleting file : $filepath"
+  rm -f $filepath
+  done <<< $FILES
+ else
+  log "archiving is : failure"
+ fi
 fi
